4f668e3f893a6af7269bd8122300d038a53f1f44
eoinlarkin <eoin.larkin@gmail.com>
2022-09-23 20:02:57 +0100

    feat: add ability to add new items to the database

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from google.auth.transport import requests
 from google.cloud import datastore
 import google.oauth2.id_token
 
 datastore_client = datastore.Client()
 
-# [END gae_python3_datastore_store_and_fetch_times]
-# [END gae_python38_datastore_store_and_fetch_times]
+
 app = Flask(__name__)
 
 
-# [START gae_python38_datastore_store_and_fetch_times]
-# [START gae_python3_datastore_store_and_fetch_times]
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key("visit"))
+def store_time(email, dt):
+    entity = datastore.Entity(key=datastore_client.key("User", email, "visit"))
     entity.update({"timestamp": dt})
 
     datastore_client.put(entity)
 
 
-def fetch_times(limit):
-    query = datastore_client.query(kind="visit")
+# def store_glist_item(email, dt):
+
+
+def fetch_times(email, limit):
+    ancestor = datastore_client.key("User", email)
+    query = datastore_client.query(kind="visit", ancestor=ancestor)
     query.order = ["-timestamp"]
 
     times = query.fetch(limit=limit)
@@ -43,13 +30,30 @@ def fetch_times(limit):
     return times
 
 
-# [END gae_python3_datastore_store_and_fetch_times]
-# [END gae_python38_datastore_store_and_fetch_times]
+def fetch_glist_items(email, limit):
+    ancestor = datastore_client.key("User", email)
+    query = datastore_client.query(kind="list_item", ancestor=ancestor)
+    #query.order = ["-item_name"]
+
+    items = query.fetch(limit=limit)
+
+    print('printing important status')
+    # for x in items:
+    #     print(x['important'])
+
+    # for x in items:
+    #     print(x['item_name'])
+
+    # print('printing item name status')
+    # for x in items:
+    #     print(x['item_name'])
+
+    return items
 
 
-# [START gae_python38_datastore_render_times]
-# [START gae_python3_datastore_render_times]
 firebase_request_adapter = requests.Request()
+
+
 @app.route("/")
 def root():
     # Verify Firebase auth.
@@ -68,30 +72,80 @@ def root():
             claims = google.oauth2.id_token.verify_firebase_token(
                 id_token, firebase_request_adapter
             )
+
+            store_time(
+                claims["email"],
+                datetime.datetime.now(tz=datetime.timezone.utc),
+            )
+            times = fetch_times(claims["email"], 10)
+            items = fetch_glist_items(claims["email"], 10)
+
+            # print(vars(times))
+            # print(vars(items))
+
+
+
+            # for x in items:
+            #     print(x['item_name'])
+            #     print(x['important'])
+
+
+
         except ValueError as exc:
             # This will be raised if the token is expired or any other
             # verification checks fail.
             error_message = str(exc)
-
-        # Record and fetch the recent times a logged-in user has accessed
-        # the site. This is currently shared amongst all users, but will be
-        # individualized in a following step.
-        store_time(datetime.datetime.now(tz=datetime.timezone.utc))
-        times = fetch_times(10)
 
     return render_template(
         "index.html",
         user_data=claims,
         error_message=error_message,
         times=times,
+        items=items,
     )
 
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
+@app.route("/add_grocery_item", methods=["GET", "POST"])
+def add_glist_item():
+    id_token = request.cookies.get("token")
+    error_message = None
+    if request.method == "POST":
 
+        if id_token:
+            try:
+                # Verify the token against the Firebase Auth API. This example
+                # verifies the token on each page load. For improved
+                # performance, some applications
+                # may wish to cache results in an encrypted
+                # session store (see for instance
+                # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+                claims = google.oauth2.id_token.verify_firebase_token(
+                    id_token, firebase_request_adapter
+                )
+
+            except ValueError as exc:
+                # This will be raised if the token is expired or any other
+                # verification checks fail.
+                error_message = str(exc)
+
+        print(error_message)
+
+        item = {"item_name": request.form.get("content"), "important": 0}
+        print(item["item_name"])
+
+        entity = datastore.Entity(
+            key=datastore_client.key("User", claims["email"], "list_item")
+        )
+        entity.update(
+            {"item_name": item["item_name"], "important": item["important"]}
+        )
+        datastore_client.put(entity)
+
+    return redirect(url_for("root"))
+
+
+if __name__ == "__main__":
+    # This is used when running locally only.
     # Flask's development server will automatically serve static files in
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
