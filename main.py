517e45ec34d71759993ba7eb8a2c75af46746835
eoinlarkin <eoin.larkin@gmail.com>
2022-09-25 14:01:35 +0100

    feat: update login process to correct bugs; refactor code

--- main.py
+++ main.py
@@ -1,79 +1,111 @@
 import datetime
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import (
+    Flask,
+    session,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
 from google.auth.transport import requests
 from google.cloud import datastore
 import google.oauth2.id_token
 
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
 datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
-app = Flask(__name__)
 
 
 def user_authenticated():
+    """
+    Checks if user is authenticated
+    If so, returns dict with claims
+    Otherwise session is set to None
+    """
     id_token = request.cookies.get("token")
-    out = {"auth": False, "user_info": None, "err": None}
+    out = {"user_info": None, "err": None}
 
     if id_token:
         try:
-            # Verify the token against the Firebase Auth API. This example
-            # verifies the token on each page load. For improved performance,
-            # some applications may wish to cache results in an encrypted
-            # session store (see for instance
-            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            # Verify the token against the Firebase Auth API
             claims = google.oauth2.id_token.verify_firebase_token(
                 id_token, firebase_request_adapter
             )
-            out["auth"], out["user_info"] = True, claims
-            return out
-        except ValueError as exc:
-            out["err"], out["auth"] = str(exc), False
+            # set user session to user id
+            session["usr"] = claims["user_id"]
 
+            # store time of authentication check
+            store_time(
+                claims["email"],
+                datetime.datetime.now(tz=datetime.timezone.utc),
+            )
+
+            out["user_info"] = claims
+            return out
+
+        except ValueError as exc:
+            out["err"] = str(exc)
+            return out
+
+    session["usr"] = None
     return out
 
 
 def store_time(email, dt):
+    """Storing last login to Datastore"""
     entity = datastore.Entity(key=datastore_client.key("User", email, "visit"))
     entity.update({"timestamp": dt})
-
     datastore_client.put(entity)
 
 
-def fetch_times(email, limit):
+def fetch_times(email, limit=1):
+    """Fetching most recent login time from Datastore"""
     ancestor = datastore_client.key("User", email)
     query = datastore_client.query(kind="visit", ancestor=ancestor)
     query.order = ["-timestamp"]
-
-    times = query.fetch(limit=limit)
+    times = query.fetch(limit)
 
     return times
 
 
-def fetch_glist_items(email, limit_1=False):
+def fetch_glist_items(email):
+    """
+    Fetching grocery list times from Datastore
+    Items are returned sorted by done, priority, name
+    """
     ancestor = datastore_client.key("User", email)
     query = datastore_client.query(kind="list_item", ancestor=ancestor)
-    # query.order = ["-item_name"]
+    query.order = ["done", "-important", "item_name"]
+    # query.order = ["-item_name","-important","-done"]
 
-    if limit_1:
-        items = query.fetch(1)
-    else:
-        items = query.fetch()
-
+    items = query.fetch()
     return items
 
 
-@app.route("/login")
-def login():
-    # Verify Firebase auth.
-    check_auth = user_authenticated()
+def switch_status(property, item_id, email):
+    """Updating importance / done status"""
 
-    if check_auth["auth"] is True:
-        return redirect(url_for("root"))
+    # get entity
+    entity = datastore_client.get(
+        datastore_client.key("User", email, "list_item", int(item_id))
+    )
 
-    return render_template("login.html")
+    # checking status and switching
+    if entity[property] == 1:
+        new_val = 0
+    else:
+        new_val = 1
+
+    entity.update({property: new_val})
+    datastore_client.put(entity)
 
 
 @app.route("/")
 def root():
+    """Render root; user redirected to login if not authenticated"""
     error_message = None
     times = None
     items = None
@@ -81,119 +113,110 @@ def root():
     # Verify Firebase auth.
     check_auth = user_authenticated()
 
-    if check_auth["auth"] is True:
-        store_time(
-            check_auth["user_info"]["email"],
-            datetime.datetime.now(tz=datetime.timezone.utc),
-        )
-        times = fetch_times(check_auth["user_info"]["email"], 1)
+    if session["usr"]:
+        times = fetch_times(check_auth["user_info"]["email"])
         items = fetch_glist_items(check_auth["user_info"]["email"])
 
-    return render_template(
-        "index.html",
-        user_data=check_auth["user_info"],
-        error_message=error_message,
-        times=times,
-        items=items,
-    )
+        return render_template(
+            "index.html",
+            user_data=check_auth["user_info"],
+            error_message=error_message,
+            times=times,
+            items=items,
+        )
+
+    return redirect(url_for("login"))
+
+
+@app.route("/login")
+def login():
+    """Render root if user authenticated, root otherwise"""
+    user_authenticated()
+    if session["usr"]:
+        return redirect(url_for("root"))
+    return render_template("login.html")
 
 
 @app.route("/add_grocery_item", methods=["GET", "POST"])
 def add_glist_item():
+    """Add entity from database if user authenticated"""
 
     # Verify Firebase auth.
     check_auth = user_authenticated()
 
     if request.method == "POST":
+        item_name = request.form.get("content")
+        if item_name == "":
+            item_name = "I'm feeling lucky"
 
-        if check_auth["auth"] is True:
-            item_name = request.form.get("content")
-            if item_name == "":
-                item_name = "I'm feeling lucky"
+        item = {
+            "item_name": item_name,
+            "important": 0,
+            "done": 0,
+        }
 
-            item = {
-                "item_name": item_name,
-                "important": 0,
-                "done": 0,
-            }
-
-            entity = datastore.Entity(
-                key=datastore_client.key(
-                    "User", check_auth["user_info"]["email"], "list_item"
-                )
+        entity = datastore.Entity(
+            key=datastore_client.key(
+                "User", check_auth["user_info"]["email"], "list_item"
             )
-            entity.update(
-                {
-                    "item_name": item["item_name"],
-                    "important": item["important"],
-                    "done": item["done"],
-                }
-            )
-            datastore_client.put(entity)
-
-    return redirect(url_for("root"))
-
-
-@app.route("/delete_item/<string:item_id>")
-def delete_glist_item(item_id):
-
-    check_auth = user_authenticated()
-
-    if check_auth["auth"] is True:
-        key = datastore_client.key(
-            "User", check_auth["user_info"]["email"], "list_item", int(item_id)
         )
-        datastore_client.delete(key)
+        entity.update(
+            {
+                "item_name": item["item_name"],
+                "important": item["important"],
+                "done": item["done"],
+            }
+        )
+        datastore_client.put(entity)
 
     return redirect(url_for("root"))
-
-
-def change_status(property, item_id, email):
-
-    # get entity
-    entity = datastore_client.get(
-        datastore_client.key("User", email, "list_item", int(item_id))
-    )
-
-    # printing important value
-    if entity[property] == 1:
-        new_val = 0
-    else:
-        new_val = 1
-
-    entity.update({property: new_val})
-
-    datastore_client.put(entity)
 
 
 @app.route("/change_important_status/<string:item_id>")
 def update_important_status(item_id):
+    """Route for update Important status"""
     check_auth = user_authenticated()
-    if check_auth["auth"] is True:
-        change_status("important", item_id, check_auth["user_info"]["email"])
+    if session["usr"]:
+        switch_status("important", item_id, check_auth["user_info"]["email"])
     return redirect(url_for("root"))
 
 
 @app.route("/change_done_status/<string:item_id>")
 def update_done_status(item_id):
+    """Route for update Done status"""
     check_auth = user_authenticated()
-    if check_auth["auth"] is True:
-        change_status("done", item_id, check_auth["user_info"]["email"])
+    if session["usr"]:
+        switch_status("done", item_id, check_auth["user_info"]["email"])
+    return redirect(url_for("root"))
+
+
+@app.route("/signout")
+def signout():
+    """Route for update Done status"""
+    session["usr"] = None
+    return redirect(url_for("login"))
+
+
+@app.route("/delete_item/<string:item_id>")
+def delete_glist_item(item_id):
+    """Delete entity from database if user authenticated"""
+    check_auth = user_authenticated()
+    if session["usr"]:
+        key = datastore_client.key(
+            "User", check_auth["user_info"]["email"], "list_item", int(item_id)
+        )
+        datastore_client.delete(key)
     return redirect(url_for("root"))
 
 
 @app.route("/clear_glist", methods=["POST"])
 def clear_glist():
-
+    """Function to fully clear list"""
     check_auth = user_authenticated()
-
-    if check_auth["auth"] is True:
-
+    if session["usr"]:
         items = fetch_glist_items(check_auth["user_info"]["email"])
-
         for item in items:
             delete_glist_item(item.key.id)
-
     return redirect(url_for("root"))
 
 
--- static/script.js
+++ static/script.js
@@ -1,81 +1,88 @@
 window.addEventListener('load', function () {
 
-    // Checks for sign-out id
-    // If present will trigger action on sign-out
-    var signOutButton = document.getElementById("sign-out");
-    if(signOutButton){
-      document.getElementById('sign-out').onclick = function () {
-      firebase.auth().signOut();
-      document.cookie = "token="
-      window.location.href = "/login";
-    }}
+  function triggerSignOut() {
+    firebase.auth().signOut();
+    document.cookie = "token="
+  }
 
-  
-    // FirebaseUI config.
-    var uiConfig = {
-      signInSuccessUrl: '/',
-      signInOptions: [
-        // Comment out any lines corresponding to providers you did not check in
-        // the Firebase console.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        //firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  
-      ],
-      // Terms of service url.
-      tosUrl: '<your-tos-url>'
-    };
-  
-    firebase.auth().onAuthStateChanged(function (user) {     
-      if (user) {
-        // User is signed in, so display the "sign out" button and login info.
-        //document.getElementById('sign-out').hidden = false;
-        //document.getElementById('login-info').hidden = false;
-        console.log(`Signed in as ${user.displayName} (${user.email})`);
-        user.getIdToken().then(function (token) {
-          // Add the token to the browser's cookies. The server will then be
-          // able to verify the token against the API.
-          // SECURITY NOTE: As cookies can easily be modified, only put the
-          // token (which is verified server-side) in a cookie; do not add other
-          // user information.
-          document.cookie = "token=" + token;
-        });
-      } else {       
-        // User is signed out.
-        // Initialize the FirebaseUI Widget using Firebase.
-        var ui = new firebaseui.auth.AuthUI(firebase.auth());
-        // Show the Firebase login button.
-        ui.start('#firebaseui-auth-container', uiConfig);
-        // Update the login state indicators.
-        //document.getElementById('sign-out').hidden = true;
-        //document.getElementById('login-info').hidden = true;
-        // Clear the token cookie.
-        document.cookie = "token=";
-      }
-    }, function (error) {
-      console.log(error);
-      alert('Unable to log in: ' + error)
-    });
+  // Checks for sign-out id
+  // If present will trigger action on sign-out
+  var signOutButton = document.getElementById("sign-out");
+  if (signOutButton) {
+    document.getElementById('sign-out').onclick = function () {
+      triggerSignOut();
+    }
+  }
+
+
+  var signOutButtonHam = document.getElementById("sign-out-hamburger");
+  if (signOutButtonHam) {
+    document.getElementById('sign-out-hamburger').onclick = function () {
+      triggerSignOut();
+    }
+  }
+
+
+  // FirebaseUI config.
+  var uiConfig = {
+    signInSuccessUrl: '/',
+    signInOptions: [
+      // Comment out any lines corresponding to providers you did not check in
+      // the Firebase console.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+  };
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in, so create the cookie token.
+      user.getIdToken().then(function (token) {
+        // Add the token to the browser's cookies. The server will then be
+        // able to verify the token against the API.
+        // SECURITY NOTE: As cookies can easily be modified, only put the
+        // token (which is verified server-side) in a cookie; do not add other
+        // user information.
+        document.cookie = "token=" + token;
+      });
+    } else {
+      // User is signed out.
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // Show the Firebase login button.
+      ui.start('#firebaseui-auth-container', uiConfig);
+      // Update the login state indicators.
+      // Clear the token cookie.
+      document.cookie = "token=";
+    }
+  }, function (error) {
+    console.log(error);
+    alert('Unable to log in: ' + error)
   });
+});
 
 
-  // Burger menus
-document.addEventListener('DOMContentLoaded', function() {
+// Burger menus
+document.addEventListener('DOMContentLoaded', function () {
   // open
   const burger = document.querySelectorAll('.navbar-burger');
   const menu = document.querySelectorAll('.navbar-menu');
 
   if (burger.length && menu.length) {
-      for (var i = 0; i < burger.length; i++) {
-          burger[i].addEventListener('click', function() {
-              for (var j = 0; j < menu.length; j++) {
-                  menu[j].classList.toggle('hidden');
-              }
-          });
-      }
+    for (var i = 0; i < burger.length; i++) {
+      burger[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
   }
 
   // close
@@ -83,22 +90,22 @@ document.addEventListener('DOMContentLoaded', function() {
   const backdrop = document.querySelectorAll('.navbar-backdrop');
 
   if (close.length) {
-      for (var i = 0; i < close.length; i++) {
-          close[i].addEventListener('click', function() {
-              for (var j = 0; j < menu.length; j++) {
-                  menu[j].classList.toggle('hidden');
-              }
-          });
-      }
+    for (var i = 0; i < close.length; i++) {
+      close[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
   }
 
   if (backdrop.length) {
-      for (var i = 0; i < backdrop.length; i++) {
-          backdrop[i].addEventListener('click', function() {
-              for (var j = 0; j < menu.length; j++) {
-                  menu[j].classList.toggle('hidden');
-              }
-          });
-      }
+    for (var i = 0; i < backdrop.length; i++) {
+      backdrop[i].addEventListener('click', function () {
+        for (var j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle('hidden');
+        }
+      });
+    }
   }
 });
